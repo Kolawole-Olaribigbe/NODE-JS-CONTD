66d5b11e8ce6ab6fb1d81fdc7a350542d5802a50
theflyestnerd <kolaolari@gmail.com>
2025-06-25 18:37:55 +0100

    REDIS CONCEPTS Data Structures

--- REDIS/data-structures.js
+++ REDIS/data-structures.js
@@ -37,11 +37,59 @@ async function redisDataStructures() {
         // const remainingNotes = await client.lRange("notes", 0, -1);
         // console.log(remainingNotes, "remainingNotes");
 
-        //sets -> SADD, SMEMBERS, SISMEMBER, SREM
-        await client.sAdd("user:nickName", ["john", "varun", "xyz"]);
-        const extractUserNicknames = client.sMembers("user:nickName");
-        console.log(extractUserNicknames);
+        //Sets -> SADD, SMEMBERS, SISMEMBER, SREM
+        // await client.sAdd("user:nickName", ["john", "varun", "xyz"]);
+        // const extractUserNicknames = client.sMembers("user:nickName");
+        // console.log(extractUserNicknames);
 
+        // const isVarunOneOfUserNicknames = await client.sIsMember("user:nickname","varun");
+        // console.log(isVarunOneOfUserNicknames);
+
+        // await client.sRem("user:nickname", 'xyz');
+        // const getUpdatedUserNickname = await client.sMembers("user:nickname");
+        // console.log(getUpdatedUserNickname);
+
+
+        //sorted sets
+        //ZADD, ZRANGE, ZRANK, ZREM
+        // await client.zAdd('cart', [
+        //     {
+        //         score : 100, 
+        //         value : 'cart 1'
+        //     },
+        //     {
+        //         score : 150, 
+        //         value : 'cart 2'
+        //     },
+        //     {
+        //         score : 10, 
+        //         value : 'cart 3'
+        //     }
+        // ]);
+        //  const getTopCartItems = await client.zRange("cart", 0, -1);
+        //  console.log(getTopCartItems);
+
+        //  const extractAllCartItemsWithScore = await client.zRangeWithScores('cart', 0, -1);
+        //  console.log(extractAllCartItemsWithScore);
+
+        //  const cartTwoRank = await client.zRank("cart", "cart 2");
+        //  console.log(cartTwoRank);
+
+        //Hashes -> HSET, HGET, HGETALL, HDEL
+        await client.hSet('product: 1', {
+            name : 'Product 1',
+            description : 'product one description',
+            rating : '5'
+        });
+        const getProductRating = await client.hGet('product: 1', 'rating');
+        console.log(getProductRating);
+
+        const getProductDetails = await client.hGetAll('product: 1');
+        console.log(getProductDetails);
+
+        await client.hDel('product: 1', 'rating');
+        const updatedProductDetails = await client.hGetAll('product: 1');
+        console.log(updatedProductDetails);
     } catch (e) {
         console.error(e)
     } finally {
